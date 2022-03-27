d0f01b3562b402e76a471c4941f010c7ce288edf
Andrew Rosário <andrew.arosario@gmail.com>
2022-03-27 17:53:11 -0300

    add config module

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(config: ConfigService) {
     super({
       datasources: {
         db: {
-          url: 'postgresql://andrewrosario:d@localhost:5432/andrewrosario?schema=public',
+          url: config.get('DATABASE_URL'),
         },
       },
     });
