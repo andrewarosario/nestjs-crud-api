75ad327fe0545aa7c5bb9e65c4b599ab06802d2f
Andrew Rosário <andrew.arosario@gmail.com>
2022-03-28 15:25:50 -0300

    returns payload in user route

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,12 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
 
 @Controller('users')
 export class UserController {
   @UseGuards(AuthGuard('jwt'))
   @Get('me')
-  getMe() {
-    return 'info user';
+  getMe(@Req() req: Request) {
+    return req.user;
   }
 }
