87cc0f5ea2b7814aec297cb7673411f76219cff6
Andrew Rosário <andrew.arosario@gmail.com>
2022-03-28 16:03:06 -0300

    add custom data attribute to ge user decorator

--- src/auth/decorators/get-user.decorator.ts
+++ src/auth/decorators/get-user.decorator.ts
@@ -1,8 +1,11 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const GetUser = createParamDecorator(
-  (data: unknown, context: ExecutionContext) => {
+  (data: string | undefined, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest();
+    if (data) {
+      return request.user[data];
+    }
     return request.user;
   },
 );
