7fb7efa79e70c9590528612cced0bd0923b076a2
Andrew Rosário <andrew.arosario@gmail.com>
2022-03-29 13:50:05 -0300

    add edit user route

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -4,6 +4,7 @@ import { AppModule } from '../src/app.module';
 import { PrismaService } from '../src/prisma/prisma.service';
 import * as pactum from 'pactum';
 import { AuthDto } from 'src/auth/dto';
+import { EditUserDto } from 'src/user/dto';
 
 describe('App e2e', () => {
   let app: INestApplication;
@@ -106,7 +107,22 @@ describe('App e2e', () => {
           .expectStatus(200);
       });
     });
-    // describe('Edit user', () => {});
+
+    describe('Edit user', () => {
+      it('should edit user', () => {
+        const dto: EditUserDto = { firstName: 'User', email: 'user@mail.com' };
+        return pactum
+          .spec()
+          .patch('/users')
+          .withHeaders({
+            Authorization: 'Bearer $S{userAt}',
+          })
+          .withBody(dto)
+          .expectStatus(200)
+          .expectBodyContains(dto.email)
+          .expectBodyContains(dto.firstName);
+      });
+    });
   });
 
   // describe('Bookmarks', () => {
